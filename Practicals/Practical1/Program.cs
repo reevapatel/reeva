98f7886fdb67de2737b4da7ac4a8b7f58fd1b653
himanshu <himanshupatel.hp956@gmail.com>
2019-12-26 09:26:31 +0530

    Patterns updated

--- Practicals/Practical1/Program.cs
+++ Practicals/Practical1/Program.cs
@@ -9,6 +9,67 @@ namespace Practical1
     {
         static void Main(string[] args)
         {
+            Pattern1();
+            Pattern2();
+            Pattern3();
+            Pattern4();
+            Console.Read();
+        }
+        static void Pattern1()
+        {
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Pattern2()
+        {
+            int i, j;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+          
+        }
+        static void Pattern3()
+        {
+            int i, j;
+            for (i = 5; i >0; i--)
+            {
+                for (j = 0; j <i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static void Pattern4()
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = i; j < 5; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= (2 * i - 1); j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
